db5c6b67cb519deadafbf9c0036409e36fa9be60
Maksim Koterev <makoterev@gmail.com>
2021-08-24 01:09:56 +0300

    Added some TODO's and started making simulation class

--- main.py
+++ main.py
@@ -1,12 +1,30 @@
 #!/usr/bin/env python3
 from models import Person, Elevator
-from curses import wrapper
+from time import sleep
+import curses
 
+
+# Actually the task is the following: 
+# Make the simulation input sequence (I need to figure how
+# it will look) and then test the same
+# sequence in two different simulations,
+# then compare the results - time in which all people are transfered
+# to wanted floors
+class Simulation():
+    
+    elevators: list[Elevator]
+    persons: list[Person]
+    visual_mode: bool  # If visual mode is False calculations will be instant
+ 
 
 def main(stdscr):
-    
+    FPS = 15
+
+    stdscr.addstr("It's working!!!")
+    stdscr.refresh()
+    sleep(1)
 
 
 if __name__ == '__main__':
-    wrapper(main)
+    curses.wrapper(main)
 
