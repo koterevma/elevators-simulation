9bb8371ca27fd2181413b0fb475f454756d43db7
Maksim Koterev <makoterev@gmail.com>
2021-08-24 11:15:26 +0300

    Refactored code to fit PEP8

--- models.py
+++ models.py
@@ -1,8 +1,9 @@
 """
-TODO: 
-    Lifts should have height variable that represents how far they are above the ground. 
-    It should be the main var and "floor" var will be calculated with it. 
-    Also make a settings.py file where global vars such as "ONE_FLOOR_HEIGHT" will be declared. 
+TODO:
+    Lifts should have height variable that represents how far they are above
+    the ground. It should be the main var and "floor" var will be calculated
+    with it. Also make a settings.py file where global vars such as
+    "ONE_FLOOR_HEIGHT" will be declared.
 """
 from typing import Optional
 
@@ -13,7 +14,6 @@ class Person():
     weight: int
     current_floor: int
     floor_dest: int  # The number of the floor that the person wants to get to
-
 
     def __init__(self, weight: int, current_floor: int, floor_dest: int):
         self.weight = weight
@@ -31,20 +31,19 @@ class Elevator():
     max_floor: int  # Top floor
     min_floor: int = 1
 
-
-    def __init__(self, max_capacity: int, max_floor: int, min_floor: Optional[int] = None, speed: Optional[float] = None):
+    def __init__(self, max_capacity: int,
+                 max_floor: int, min_floor: Optional[int] = None,
+                 speed: Optional[float] = None):
         self.max_capacity = max_capacity
         self.current_floor = 1
         self.speed = 1.0  # m/s
         self.max_floor = max_floor
-        if speed != None:
+        if speed is not None:
             self.speed = speed
-
 
     def __str__(self):
         """String representation for console drawing"""
         return """TODO [elevator]
-    representation
-    bsn
-    newline"""
-
+representation
+bsn
+newline"""
