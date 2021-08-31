d2ade9db1a6009d8a5b41c5666cec397bf14cc28
Maksim Koterev <makoterev@gmail.com>
2021-08-31 18:21:39 +0300

    Tests now fit into one function

--- test_models.py
+++ test_models.py
@@ -4,23 +4,21 @@ from models import Elevator, Person
 
 class ModelsTest(unittest.TestCase):
 
-    def test_can_fit_false_1(self):
+    def test_can_fit(self):
+
         self.elevator = Elevator(10, 10)
         self.elevator.passangers.append(Person(10, 1, 10))
         self.assertFalse(self.elevator.can_fit(Person(1, 1, 1)))
 
-    def test_can_fit_false_2(self):
+        self.elevator = Elevator(10, 10)
+        self.elevator.passangers.append(Person(9, 1, 10))
+        self.assertTrue(self.elevator.can_fit(Person(1, 1, 1)))
+
         self.elevator = Elevator(30, 10)
         self.elevator.passangers.append(Person(10, 1, 10))
         self.elevator.passangers.append(Person(20, 1, 10))
         self.assertFalse(self.elevator.can_fit(Person(1, 1, 1)))
 
-    def test_can_fit_true_1(self):
-        self.elevator = Elevator(11, 10)
-        self.elevator.passangers.append(Person(10, 1, 10))
-        self.assertTrue(self.elevator.can_fit(Person(1, 1, 1)))
-
-    def test_can_fit_true_2(self):
         self.elevator = Elevator(30, 10)
         self.elevator.passangers.append(Person(10, 1, 10))
         self.elevator.passangers.append(Person(19, 1, 10))
